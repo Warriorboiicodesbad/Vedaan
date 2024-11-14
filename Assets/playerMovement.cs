b0148c712298fbd2df3344d62e97d7e6869a10b6
Warriorboiicodesbad <163497115+Warriorboiicodesbad@users.noreply.github.com>
2024-11-14 18:33:57 +0530

    fixed even more

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -5,25 +5,22 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float moveSpeed;
-    public Transform orientation;
-    float horizontalInput;
-    float verticalInput;
-    Vector3 moveDirection;
-    Rigidbody rb;
-    public CharacterController controller;
+ 
+   
+    
+   
+   
     public float speed = 12f;
     public float mouseSensitivity = 100f;
-    public Transform playerBody;
+    
 
     private float xRotation = 0f;
-    private float yRotation = 0f;
+    
     public CinemachineVirtualCamera virtualcamera;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        
 
 
         Cursor.lockState = CursorLockMode.Locked;
@@ -35,7 +32,7 @@ public class playerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        
 
         
         transform.Translate(x * Time.deltaTime * speed, 0, z * Time.deltaTime * speed, Space.Self);
