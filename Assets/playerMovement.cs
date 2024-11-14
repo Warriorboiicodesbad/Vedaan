c2f228886883c0fd05e44f100c2ca8b248ff6bde
Warriorboiicodesbad <163497115+Warriorboiicodesbad@users.noreply.github.com>
2024-11-14 18:21:51 +0530

    fixed movement and rotation

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -1,38 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    public float moveSpeed;
+    public Transform orientation;
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDirection;
+    Rigidbody rb;
+    public CharacterController controller;
+    public float speed = 12f;
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
 
-    public float speed = 3.0f;
-    // Start is called before the first frame update
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+    public CinemachineVirtualCamera virtualcamera;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(transform.forward * speed * Time.deltaTime);
-        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        if  (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(transform.forward * -speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(transform.right * -speed * Time.deltaTime);
-        }
+        Vector3 move = transform.right * x + transform.forward * z;
 
-        if  (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(transform.right * speed * Time.deltaTime);
-        }
         
+        transform.Translate(x * Time.deltaTime * speed, 0, z * Time.deltaTime * speed, Space.Self);
+
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        virtualcamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
+        transform.Rotate(Vector3.up * mouseX);
+
+
+
+
     }
+    
 }
+
